72406bd7f290865be845f3a81e5961710906e832
Sunny Raj <111192197+sunny8540@users.noreply.github.com>
2022-11-13 11:29:37 +0530

    Updates

---
+++ precache-manifest.317bfbc8c56e3010b6ee8361c7923e82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64276cd5c1cca4ea1da2",
+    "url": "/tradeApp/static/css/main.928d900c.chunk.css"
+  },
+  {
+    "revision": "64276cd5c1cca4ea1da2",
+    "url": "/tradeApp/static/js/main.64276cd5.chunk.js"
+  },
+  {
+    "revision": "1349020c76722c642673",
+    "url": "/tradeApp/static/js/1.1349020c.chunk.js"
+  },
+  {
+    "revision": "ba3fbb52c907ed16ef47",
+    "url": "/tradeApp/static/js/runtime~main.ba3fbb52.js"
+  },
+  {
+    "revision": "b874f494825b55dadf4ccefa67c84bd4",
+    "url": "/tradeApp/index.html"
+  }
+];
